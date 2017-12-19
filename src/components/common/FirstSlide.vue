96335dca8ba65ad10659a2051a44f065dc286ae0
shanshan <1044917411@qq.com>
2017-12-19 23:54:03 +0800

    首页完毕

--- src/components/common/FirstSlide.vue
+++ src/components/common/FirstSlide.vue
@@ -1,6 +1,6 @@
 <template>
-	<div class="container slide">
-		<swiper :options="swiperOption" ref="mySwiper">
+	<div class="slide firstSlide">
+		<swiper :options="swiperOption" ref="mySwiper" :style='{height:clientHeight+"px"}'>
 			<swiper-slide>
 				<a href="" class="img_1"></a>
 			</swiper-slide>
@@ -10,7 +10,8 @@
       <swiper-slide>
 				<a href="" class="img_3"></a>
 			</swiper-slide>
-			<div class="swiper-pagination" slot="pagination"></div>
+			<div class="swiper-pagination" slot="pagination">
+      </div>
 		</swiper>
 	</div>
 </template>
@@ -19,25 +20,34 @@ export default{
 	name:'slide',
 	data:function () {
       return {
+        clientHeight: window.document.body.offsetHeight,
         swiperOption: {
           loop: true,
           autoplay: true,
           // slideToClickedSlide:true,
-          paginationClickable :true, 
           pagination: {
             el: '.swiper-pagination',
-            paginationClickable :true,
+            clickable: true,
+            bulletActiveClass: 'pagination-current',
+            renderBullet: function (index, className) {
+              return `<span class=${className} style="opacity:0.8;width:20px;height:3px;background:#666;margin:0 3px;"></span>`
+            }
           },
         }
       }
     },
+    mounted(){
+      if(this.clientHeight > 768) {
+        this.clientHeight = 768
+      }
+    }
 }
 </script>
-<style scoped lang="less">
-  .container{
-     height:886px;
+<style lang="less">
+  .firstSlide{
+     height:686px;
     .swiper-slide{
-      height:886px;
+      height:100%;
         a{
           display: block;
           height: 100%;
@@ -52,6 +62,16 @@ export default{
         a.img_3{
           background:url("../../assets/img/u1kvpc1_c0211bb6.jpg") no-repeat center;  
         }
+     }
+     .swiper-pagination{
+       height: 20px;
+       line-height: 20px;
+       display: flex;
+       justify-content: center;
+       align-items: center;
+     }
+     .pagination-current{
+       background: red!important;
      }
   }
 </style>
--- src/components/common/Header.vue
+++ src/components/common/Header.vue
@@ -43,29 +43,29 @@
             <a href="">服务</a>
             <!-- 服务下列表 -->
             <div class="server-box">
-               <ul class="clear server_ul">
-                 <li>
-                     <div class="service-care1"></div>
-                     <div>
-                       <p>NIU CARE</p>
-                       <p>保养计划</p>
-                     </div>
-                 </li>
-                 <li>
-                     <div class="service-care2"></div>
-                     <div>
-                       <p>牛油保</p>
-                       <p>人车保险</p>
-                     </div>
-                 </li>
-                 <li>
-                     <div class="service-care3"></div>
-                     <div>
-                       <p>线下门店</p>
-                       <p>查找线下服务</p>
-                     </div>
-                 </li>
-               </ul>
+              <ul class="clear server_ul">
+                <li>
+                  <div class="service-care1 server-ico" ></div>
+                  <div class="details">
+                    <p>NIU CARE</p>
+                    <p>保养计划</p>
+                  </div>
+                </li>
+                <li>
+                  <div class="service-care2 server-ico"></div>
+                  <div class="details">
+                    <p>牛油保</p>
+                    <p>人车保险</p>
+                  </div>
+                </li>
+                <li>
+                  <div class="service-care3 server-ico"></div>
+                  <div class="details">
+                    <p>线下门店</p>
+                    <p>查找线下服务</p>
+                  </div>
+                </li>
+              </ul>
             </div>
           </div>
         </li>
@@ -91,123 +91,77 @@
 </template>
 
 <script>
-export default {
-  name: "HelloWorld",
-  data() {
-    return {
-      isShow: false
-    };
-  },
-  monthds: {
-   
-  }
-};
+  export default {
+    name: "HelloWorld",
+    data () {
+      return {
+        isShow: false,
+      };
+    },
+    monthds: {
+
+    }
+  };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang='less'>
-.content {
-  margin-top: 30px;
-  left: 50%;
-  transform: translatex(-50%);
-  top: 0;
-  background-color: #ffffff;
-  border-radius: 3px;
-  height: 50px;
-  line-height: 50px;
-  position: absolute;
-  z-index: 10;
-  text-align: center;
-  div.header-logo {
-    a {
-      display: inline-block;
-      height: 50px;
-      padding: 0 20px;
-      img {
-        width: 100%;
-        vertical-align: middle;
-      }
-    }
-  }
-  nav {
-    margin: 0 60px 0 30px;
-    ul {
-      width: 100%;
-      height: 100%;
-      li {
-        float: left;
-        margin: 0px 10px;
-        width: 90px;
-        cursor: pointer;
-        a {
-          height: 50px;
-          width: 100%;
-          display: inline-block;
-        }
+  .content {
+    margin-top: 30px;
+    left: 50%;
+    transform: translatex(-50%);
+    top: 0;
+    background-color: #ffffff;
+    border-radius: 3px;
+    height: 50px;
+    line-height: 50px;
+    position: absolute;
+    z-index: 10;
+    text-align: center;
+    div.header-logo {
+      a {
+        display: inline-block;
+        height: 50px;
+        padding: 0 20px;
         img {
-          width: 30px;
-          height: 30px;
+          width: 100%;
           vertical-align: middle;
         }
       }
-      li.li_car{
-        //下列车图
-        .header-car {
-          position: relative;
-          .car-box{
-            position: absolute;
-            display: none;
-            background: #fff;
-            width: 1170px;
-            height: 240px;
-            top: 50px;
-            left: -142px;
-            ul {
-              display: flex;
-              width: 100%;
-              justify-content: space-between;
-              text-align: center;
-              li {
-                height: 100%;
-                width: 300px;
-                div {
-                  width: 100%;
-                  height: 100%;
-                  a {
-                    display: block;
-                    height: 180px;
-                    img {
-                      width: 100%;
-                      height: 180px;
-                    }
-                  }
-                  p {
-                    font-size: 16px;
-                    font-weight: bold;
-                    padding: 0 5px;
-                    cursor: pointer;
-                  }
-                }
-              }
-            }
+    }
+    nav {
+      margin: 0 60px 0 30px;
+      ul {
+        width: 100%;
+        height: 100%;
+        li {
+          float: left;
+          margin: 0px 10px;
+          width: 90px;
+          cursor: pointer;
+          a {
+            height: 50px;
+            width: 100%;
+            display: inline-block;
+          }
+          img {
+            width: 30px;
+            height: 30px;
+            vertical-align: middle;
           }
         }
-      }
-      li.li_car:hover .header-car{
-        display:block !important;
-      }
-      li.li_server{
-          .header-server {
+        li.li_car {
+          //下列车图
+          .header-car {
             position: relative;
-            .server-box{
+            .car-box {
               position: absolute;
               display: none;
               background: #fff;
               width: 1170px;
               height: 240px;
               top: 50px;
-              left: -142px;
-              border:1px solid red;
+              left: -140px;
               ul {
                 display: flex;
                 width: 100%;
@@ -238,20 +192,90 @@ export default {
               }
             }
           }
+          &:hover .car-box {
+            display: block;
+          }
+        }
+        li.li_server {
+          .header-server {
+            position: relative;
+            .server-box {
+              position: absolute;
+              display: none;
+              background: #fff;
+              width: 1170px;
+              height: 240px;
+              top: 50px;
+              left: -470px;
+              ul {
+                display: flex;
+                width: 100%;
+                justify-content: space-between;
+                text-align: center;
+                li {
+                  height: 50%;
+                  width: 300px;
+                  padding:50px 50px;
+                  display: flex;
+                  div.server-ico {
+                    width: 100%;
+                    background-size: inherit;
+                    width: 100px;
+                    height: 72px;
+                    line-height: 72px;
+                    margin-top: 19px;
+                    margin-left: 30px;
+                    text-align: center;
+                    a {
+                      display: block;
+                      height: 180px;
+                      img {
+                        width: 100%;
+                        height: 180px;
+                      }
+                    }
+                    p {
+                      font-size: 16px;
+                      font-weight: bold;
+                      padding: 0 5px;
+                      cursor: pointer;
+                    }
+                  }
+                  div.service-care1{
+                    background:url('../../assets/img/service-care_f1ff6a8c.svg') no-repeat center;
+                  }
+                  div.service-care2{
+                    background:url('../../assets/img/service-insurence_f99fa20c.svg') no-repeat center;
+                  }
+                  div.service-care3{
+                    background:url('../../assets/img/service-locate_9841d7c4.svg') no-repeat center;
+                  }
+                  div.details{
+                    margin-left: 10px;
+                    text-align: left;
+                    font-size: 14px;
+                    color: #a4a9ad;
+                  }
+                }
+              }
+            }
+          }
+          &:hover .server-box {
+            display: block;
+          }
         }
       }
     }
-  .header-right {
-    div {
-      float: left;
-      margin: 0px 10px;
-      a {
-        display: block;
-        height: 50px;
+    .header-right {
+      div {
         float: left;
+        margin: 0px 10px;
+        a {
+          display: block;
+          height: 50px;
+          float: left;
+        }
       }
     }
   }
-}
-
 </style>
